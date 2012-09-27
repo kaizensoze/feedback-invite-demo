c807d52478cbef3cc5783952ced10663007826b8
Joe Gallo <gallo.j@gmail.com>
2012-09-27 16:39:43 -0400

    Further simplifying/reducing js. Working on thank you pages.

--- js/invite-feedback.js
+++ js/invite-feedback.js
@@ -5,25 +5,52 @@ $(document).ready(function() {
   });
 
   $('#invite-button').click(function() {
-    $('#feedback-content').hide();
-    $('#invite-content').toggle();
-
-    $('#feedback-button').removeClass('active');
-    $(this).toggleClass('active');
+    if (!$(this).hasClass('active')) {  // Opening.
+      feedback_close();
+      invite_open();
+    } else {  // Closing.
+      invite_close();
+    }
   });
 
   $('#feedback-button').click(function() {
-    $('#invite-content').hide();
-    $('#feedback-content').toggle();
-
-    $('#invite-button').removeClass('active');
-    $(this).toggleClass('active');
+    if (!$(this).hasClass('active')) {  // Opening.
+      invite_close();
+      feedback_open();
+    } else {  // Closing.
+      feedback_close();
+    }
   });
 
-  function close() {
+  $('#invite-submit').click(function() {
     $('#invite-content').hide();
-    $('#feedback-content').hide();
+    $('#invite-thankyou').show();
+  })
+
+  function invite_open() {
+    $('#invite-thankyou').hide();
+    $('#invite-content').show();
+    $('#invite-button').addClass('active');
+  }
+  function invite_close() {
+    $('#invite-thankyou').hide();
+    $('#invite-content').hide();
     $('#invite-button').removeClass('active');
+  }
+
+  function feedback_open() {
+    $('#feedback-thankyou').hide();
+    $('#feedback-content').show();
+    $('#feedback-button').addClass('active');
+  }
+  function feedback_close() {
+    $('#feedback-thankyou').hide();
+    $('#feedback-content').hide();
     $('#feedback-button').removeClass('active');
+  }
+
+  function close() {
+    invite_close();
+    feedback_close();
   }
 });
